6a508231283faa7d1522f3e237c4c3a7a48f8c41
Dmitry <dn1123667@yandex.ru>
2022-09-05 04:15:47 +0300

    1задание

---
+++ 1cem/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Ведите первое число");
+string number1;
+number1 = Console.ReadLine();
+int number1a;
+number1a = int.Parse(number1);
+
+System.Console.WriteLine("Ведите второе число");
+string number2;
+number2 = Console.ReadLine();
+int number2a;
+number2a = int.Parse(number2);
+Console.WriteLine("Наибольшее число");
+
+if (number1a>number2a)
+{
+
+    Console.WriteLine(number1);
+}
+else
+{
+   Console.WriteLine(number2);   
+}
